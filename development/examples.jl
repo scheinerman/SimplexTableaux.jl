61d55f2c4abe20fa0f4af575baaccc07c77b86d6
Ed Scheinerman <ers@jhu.edu>
2025-07-24 20:34:42 -0400

    more examples

--- development/examples.jl
+++ development/examples.jl
@@ -1,4 +1,4 @@
-using SimplexTableaux
+using SimplexTableaux, SimpleDrawing, Plots, SimpleDrawingObjects
 
 """
     dog_food()
@@ -42,6 +42,52 @@ function small_example()
     b = [1; 6]
     c = [1; 2; 3]
     return Tableau(A, b, c, false)
+end
+
+function unbounded_example()
+    A = [1 2; 2 1; 0 -1]
+    b = [5; 4; -3]
+    c = [-1; -2]
+    T = Tableau(A, b, c)
+    return T
+end
+
+function unbounded_picture()
+    newdraw()
+    S = Segment(2.5im, 5+0im)
+    set_linewidth!(S, 2)
+
+    draw(S)
+
+    S = Segment(2, 0, 0, 4)
+    set_linewidth!(S, 2)
+    draw(S)
+
+    S = Segment(0, 3, 6, 3)
+    set_linewidth!(S, 2)
+
+    draw(S)
+
+    draw_xaxis(-1, 6)
+    draw_xtick(1:5)
+
+    draw_yaxis(-1, 5)
+    draw_ytick(1:4)
+
+    finish()
+end
+
+function degenerate_example()
+    A = [
+        7 7 45 -1 3 -53 -68
+        9 -5 27 -115 7 -129 42
+        5 -3 63 -96 10 -109 86
+    ]
+    b = [26; 18; 34]
+    c = [1; 7; -37; 94; -9; 76; -146]
+    T = Tableau(A, b, c, false)
+    set_basis!(T, [1, 3, 6])
+    return T
 end
 
 nothing
