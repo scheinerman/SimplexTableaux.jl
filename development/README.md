e75d49030ec6af6ea37eb3b70c1136d10ac30049
Ed Scheinerman <ers@jhu.edu>
2025-08-12 16:32:16 -0400

    Docs, bug fixes

---
+++ development/README.md
@@ -0,0 +1,5 @@
+# Development Folder for `SimplexTableaux`
+
+This is not part of the "official" release of `SimplexTableaux` but contains 
+code to generate various examples for working with this module. Code may be added or deleted
+without notice. Changes here are not considered to be breaking. 
--- development/random-examples.jl
+++ development/random-examples.jl
@@ -24,6 +24,11 @@ Compute the solution to the LP in `T` and make sure its correct.
 function check_solution(T::Tableau)::Bool
     T = deepcopy(T)
     x = simplex_solve!(T, false)
+
+    if isnothing(x)
+        return true
+    end
+
     if any(T.A * x .> T.b)
         return false
     end
@@ -32,11 +37,11 @@ function check_solution(T::Tableau)::Bool
         return false
     end
 
-    if dot(T.c, x) != T.M[end, end]
+    if dot(T.c, x) != T.M[1, end]
         return false
     end
 
-    xx = lp_solve(T)
+    xx = lp_solve(T, false)
     if norm(Float64.(x) - xx) > 1e-6
         return false
     end
@@ -78,12 +83,12 @@ end
 """
     find_bad(nv, nc, m=100, reps=100)
 
-Try to find a failure for `pivot_solve`.
+Try to find a failure for `simplex_solve!`.
 """
-function find_bad(nv, nc, m=100, reps=100)
+function find_bad(nv, nc, lo=0, hi=20, reps=100)
     P = Progress(reps)
     for _ in 1:reps
-        T = random_example(nv, nc, m)
+        T = random_example(nv, nc, lo, hi)
         if !check_solution(T)
             @info "Found a bad example"
             return T
